ffb493f69618fcc09815f5e6e010713c1437eb0c
saurabh <saurabh956@gmail.com>
2020-02-12 00:03:15 +0530

    weather api data fetch

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    api_request = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=47c2bd40b1ec167becfe5ac513b337cf")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = 'Error.........'
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
