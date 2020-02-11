80f8793aa16ebd3114f77174ffacdaaae5515036
saurabh <saurabh956@gmail.com>
2020-02-11 21:06:23 +0530

    updated base.html commit

--- lookup/urls.py
+++ lookup/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name = 'home'),
+    path('about/', views.about, name = 'about'),
 ]
